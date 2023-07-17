c013ad224d1e92aae3a92d4476b7ebe12e7a6fb3
CletosMarcos <cletosmarcos@gmail.com>
2023-07-16 17:33:16 -0700

    fix betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main()- start point
+ * Description: print double quotes
+ * Return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
