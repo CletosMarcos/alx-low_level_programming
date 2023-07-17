c70868f4c514cbdbefacc11e5d26135e9b969cea
CletosMarcos <cletosmarcos@gmail.com>
2023-07-17 15:22:07 -0700

    add file 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - entry point
+ * Description: print to standard error
+ * Return : 1 (Success)
+ */
+
+int main(void)
+{
+	const char* msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	fwrite(msg, sizeof(char), strlen(msg), stderr);
+	
+	return (1);
+}
