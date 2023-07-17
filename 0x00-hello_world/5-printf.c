7bf71290f01bde7df514b448fbfc023bf381c3c1
CletosMarcos <cletosmarcos@gmail.com>
2023-07-16 17:41:45 -0700

    add file 5 | use printf() to print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - execution start point
+ * return: return integer 0 if successful
+ */
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
