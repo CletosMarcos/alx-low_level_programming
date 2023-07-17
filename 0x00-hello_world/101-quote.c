5d6505cd78a971486ed3952b1f1a962223a84ff4
CletosMarcos <cletosmarcos@gmail.com>
2023-07-17 15:52:23 -0700

    fix betty-style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <string.h>
+
 /**
  * main - entry point
  * Description: print to standard error
- * Return : 1 (Success)
+ * Return: 1 (Success)
  */
 
 int main(void)
 {
-	const char* msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char *msg = "and that piece of art is useful\"
+- Dora Korpar, 2015-10-19\n";
+
 	fwrite(msg, sizeof(char), strlen(msg), stderr);
-	
+
 	return (1);
 }
