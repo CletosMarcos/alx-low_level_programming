603e625541b62c1580e954678f484317add6224c
CletosMarcos <cletosmarcos@gmail.com>
2023-07-18 17:53:43 -0700

    add file 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: print all single digit numbers of base 10
+ * 	starting from 0 using putchar(), ascii
+ * Return: 0 (SUCCESS)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
