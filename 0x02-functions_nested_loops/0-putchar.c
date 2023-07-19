0447590f470d12281de4669995f6eafda7d9c715
CletosMarcos <cletosmarcos@gmail.com>
2023-07-18 18:33:07 -0700

    add file 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
