ab311ba4e2146299019dc093756ee1181d980acc
CletosMarcos <cletosmarcos@gmail.com>
2023-08-19 16:53:53 -0700

    add file 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet - print the alphabet in lowercase
+ * Desccription: use functions
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char az = 'a';
+
+	while(az < 'z')
+	{
+		_putchar(az);
+		az++;
+	}
+	_putchar('\n');
+}
