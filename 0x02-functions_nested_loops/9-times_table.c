dee9fb42d1f878bc8b683dd600f25b13187559bf
CletosMarcos <cletosmarcos@gmail.com>
2023-08-20 19:57:21 -0700

    review task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,7 +8,7 @@
 
 void times_table(void)
 {
-	char i = 0, res, j;
+	int i = 0, res, j;
 
 	while (i <= 9)
 	{
@@ -23,22 +23,19 @@ void times_table(void)
 				_putchar('0' + res / 10);
 				_putchar('0' + res % 10);
 			}
+			else if (res != 0)
+			{
+				_putchar(' ');
+				_putchar('0' + res);
+			}
 			else
 				_putchar('0' + res);
 			if (j != 9)
 			{
-				if (res <= 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
+				_putchar(',');
+				_putchar(' ');
 			}
+			
 			j++;
 		}
 		_putchar('\n');
