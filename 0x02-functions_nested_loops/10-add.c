2b23ca0185404230faf44f0ec281a4e6365d71bf
CletosMarcos <cletosmarcos@gmail.com>
2023-08-21 20:06:50 -0700

    add solution for task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two numers
+ * @n: number to be added
+ * @m: number to be added
+ * Return: the result
+ */
+
+int add(int n, int m)
+{
+	int res;
+
+	res = n + m;
+	return (res);
+}
