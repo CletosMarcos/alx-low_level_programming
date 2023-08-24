12e555c4417d0c4d34f40907c3af079edbf3c3dd
CletosMarcos <cletosmarcos@gmail.com>
2023-08-24 05:03:56 -0700

    review output

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,14 +9,14 @@
 int main(void)
 {
         int count = 2;
-        unsigned long aux = 1, aux2 = 2, res;
+        unsigned long int aux = 1, aux2 = 2, res;
         /*count = 2 considering i'm printing 1 and 2 out of the while loop*/
 
         printf("1, 2, ");
-        while (count <= 7)
+        while (count <= 97)
         {
                 res = aux + aux2;
-                (count == 7) ?
+                (count == 97) ?
                         printf("%lu", res) :
                         printf("%lu, ", res);
                 aux = aux2;
