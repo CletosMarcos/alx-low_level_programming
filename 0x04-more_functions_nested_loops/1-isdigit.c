f88079b61124299dbd41fc32862babe814310b5a
CletosMarcos <cletosmarcos@gmail.com>
2023-08-25 19:32:13 -0700

    add solution for task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * _isdigit - checks for a digit
+ * @c: input to be checked
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	int ret;
+
+	if (c >= 0 && c <= 9)
+		ret = 1;
+	else
+		ret = 0;
+	return (ret);
+}
