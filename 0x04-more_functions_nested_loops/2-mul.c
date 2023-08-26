776e23abdd2c57772ed7535c65c86b5c6eaea325
CletosMarcos <cletosmarcos@gmail.com>
2023-08-25 19:42:10 -0700

    add solution for task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * mul - multiplies teo integers
+ * @a: integer to be multiplied
+ * @b: integer to be multiplied
+ * Return: the result of the operation
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
