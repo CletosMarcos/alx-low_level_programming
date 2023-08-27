57418ec066dab39becfeab6c14964a8f255c6e31
CletosMarcos <cletosmarcos@gmail.com>
2023-08-27 16:25:50 -0700

    fix betty-doc syntax

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,7 @@
 
 /**
  * print_line - draws a straight line in the terminal
- * @n - number of times the line should be printed
+ * @n: number of times the line should be printed
  * Return: 0
  */
 
