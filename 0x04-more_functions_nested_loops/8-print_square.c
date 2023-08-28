03f126a71a005ea96d188155fb1d10f0791255c5
CletosMarcos <cletosmarcos@gmail.com>
2023-08-27 17:06:36 -0700

    fix output

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,15 +9,20 @@ void print_square(int size)
 {
 	int i = 0, j;
 
-	while (i < size)
-	{
-		j = 0;
-		while (j < size)
-		{
-			_putchar('#');
-			j++;
-		}
+	if (size <= 0)
 		_putchar('\n');
-		i++;
+	else
+	{
+		while (i < size)
+		{
+			j = 0;
+			while (j < size)
+			{
+				_putchar('#');
+				j++;
+			}
+			_putchar('\n');
+			i++;
+		}
 	}
 }
