02a828186b32cb75e00ddbe100a50644c661e7e6
CletosMarcos <cletosmarcos@gmail.com>
2024-02-21 04:31:20 -0800

    add task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - resets a value passed as parameter from a pointer to 98
+ * @n : pointer value to be changed
+ * return : Nothing
+ */
+
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
