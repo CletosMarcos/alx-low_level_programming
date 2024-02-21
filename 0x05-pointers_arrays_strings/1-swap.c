d1ccc5d80b45089c05e8c3b4b307fa4dca8191c3
CletosMarcos <cletosmarcos@gmail.com>
2024-02-21 04:56:51 -0800

    add task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the values of two Integers
+ * @a: 1st value
+ * @b 2nd value
+ * Return: Nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
+}
