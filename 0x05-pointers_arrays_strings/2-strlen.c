f14167df8eb19bed31b3bc89d398c489550c642c
CletosMarcos <cletosmarcos@gmail.com>
2024-02-22 17:46:46 -0800

    add task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to be verified
+ * Return: length
+ */
+
+int _strlen (char *s)
+{
+	int c, a = 0;
+	for(c = 0; (s[c] != '\0'); c++)
+		a++;
+	return (a);
+}
