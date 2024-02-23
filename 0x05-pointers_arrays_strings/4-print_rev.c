53b144f4516966fba41422f911694d8d238d798b
CletosMarcos <cletosmarcos@gmail.com>
2024-02-23 05:30:35 -0800

    review task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,7 +12,7 @@ void print_rev(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 		count++;
-	for (i = count-1; s[count] >= 0; count--)
-		_putchar(s[count]);
+	for (i = count; s[i] >= 0; i--)
+		_putchar(s[i]);
 	_putchar('\n');
 }
