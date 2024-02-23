41fba0a97604d736a2932d113b88ac649bb21e70
CletosMarcos <cletosmarcos@gmail.com>
2024-02-23 04:38:43 -0800

    add task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ * @s: string
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int i, count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+	for (i = count; s[count] >= 0; count--)
+		_putchar(s[count]);
+	_putchar('\n');
+}
